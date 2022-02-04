313b9e30e8b7013ff6fd39065385666f7c777ca6
daiannymaria <92277662+daiannymaria@users.noreply.github.com>
2022-02-04 18:39:06 -0300

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -3,26 +3,28 @@ let tamanho = 2;
 
         function inserir(){
 			div = document.createElement("div");
-			div.style.fontSize = `${tamanho}em`;
-			div.innerHTML = `<button onclick="aumentar()">+</button>
-            <button onclick="diminuir()">-</button>
+			div.style.fontSize = `${controle}em`;
+			div.innerHTML = `<button onclick="maior()">+</button>
+            <button onclick="menor()">-</button>
 			`;
 			document.body.prepend(div)
 		}
-
-        function atualização() {
-            const h1 = document.querySelector("h1");
-            h1.style.fontSize = `${tamanho}em`;
         }
-        function aumentar() {
-			if (tamanho < 5) {
-				tamanho += 0.5;
-				atualização();
-			}
-        }
-        function diminuir() {
-            if (tamanho > 0.5) {
-                tamanho -= 0.5;
-                atualização();
+        function menor() {
+            if (controle > 0.5) {
+                controle -= 0.5;
+                mediano();
             }
         }
+
+        function mediano() {
+            const h1 = document.querySelector("h1");
+            h1.style.fontSize = `${controle}em`;
+        }
+
+        function maior() {
+			if (controle < 5) {
+				controle += 0.5;
+				mediano();
+			}
+
